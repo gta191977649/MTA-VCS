8a95b86551f2e650759f39276674d2f0f3178fd5
ヌルポ <191977649@qq.com>
2021-12-15 22:14:17 +0800

    more neon fix

--- mods/deathmatch/resources/[graphic]/ps2/diffuse/diffuse_vcs.lua
+++ mods/deathmatch/resources/[graphic]/ps2/diffuse/diffuse_vcs.lua
@@ -1,15 +1,15 @@
-local buildingVS = dxCreateShader("fx/buildingvs_vcs.fx", 0, 0, false, "world,object,ped")
+local buildingPipeline = dxCreateShader("fx/building_vcs.fx", 0, 0, false, "world,object,ped")
 
 function enableDiffuse()
     print("VCS Diffuse Enabled")
-    engineApplyShaderToWorldTexture(buildingVS, "*")
+    engineApplyShaderToWorldTexture(buildingPipeline, "*")
     for k,v in pairs({"coronastar", "sitem16", "unnamed", "white64", "*radar*", "font1", "fist", "ak47icon", "brassknuckleicon", "golfclubicon", "nitestickicon", "knifecuricon", "baticon", "shovelicon", "poolcueicon", "katanaicon", "chnsawicon", "gun_dildo1icon", "gun_dildo2icon", "gun_vibe1icon", "floweraicon", "gun_caneicon", "grenadeicon", "teargasicon", "molotovicon", "colt45icon", "silencedicon", "desert_eagleicon", "chromegunicon", "sawnofficon", "shotgspaicon", "micro_uziicon", "mp5lngicon", "m4icon", "tec9icon", "cuntgunicon", "snipericon", "rocketlaicon", "heatseekicon", "flameicon", "minigunicon", "satchelicon", "bombicon", "spraycanicon", "fire_exicon", "cameraicon", "nvgogglesicon", "irgogglesicon", "gun_paraicon","*txgrass*","*neon*","*water*"}) do
-        engineRemoveShaderFromWorldTexture(buildingVS, v)
+        engineRemoveShaderFromWorldTexture(buildingPipeline, v)
     end
 end
 
 function disableDiffuse()
     print("VCS Diffuse Disabled")
-    engineRemoveShaderFromWorldTexture(buildingVS,"*")
+    engineRemoveShaderFromWorldTexture(buildingPipeline,"*")
 
 end
---
+++ mods/deathmatch/resources/[graphic]/ps2/fx/building_vcs.fx
@@ -0,0 +1,64 @@
+//CREDIT VCS PC EDITION TEAM
+
+//#define surfAmb (surfProps.x)
+#include "mta-helper.fx"
+
+#define surfAmb 0.6
+#define ambient gGlobalAmbient
+
+sampler Sampler0 = sampler_state
+{
+    Texture = (gTexture0);
+};
+
+struct VS_INPUT
+{
+	float4 Position		: POSITION;
+	float2 TexCoord		: TEXCOORD0;
+	float4 Color		: COLOR0;
+};
+
+struct VS_OUTPUT {
+	float4 Position		: POSITION;
+	float2 Texcoord0	: TEXCOORD0;
+	float4 Color		: COLOR0;
+};
+
+
+matrix Identity =
+{
+    { 1, 0, 0, 0 },
+    { 0, 1, 0, 0 },
+    { 0, 0, 1, 0 },
+    { 0, 0, 0, 1 }
+};
+
+VS_OUTPUT Vertex_Pipeline(in VS_INPUT IN)
+{
+	VS_OUTPUT PS;
+
+	PS.Position = mul(IN.Position, gWorldViewProjection);
+	PS.Texcoord0 = mul(Identity, float4(IN.TexCoord, 0.0, 1.0)).xy;
+
+	PS.Color = IN.Color*IN.Color;
+	PS.Color.rgb += ambient*surfAmb * 128.0/255.0;
+
+	return PS;
+}
+
+float4 Pixel_Pipeline(VS_OUTPUT PS) : COLOR0
+{
+	float4 finalColor = tex2D(Sampler0, PS.Texcoord0.xy)*PS.Color * 255.0/128.0;
+	return finalColor;
+}
+
+
+technique worldfiff
+{
+    pass P0
+    {
+        VertexShader = compile vs_2_0 Vertex_Pipeline();
+        //PixelShader = compile ps_2_0 Pixel_Pipeline();
+        
+    }
+}
