998e37bb0f5486c8c2e7584bc876c920cc20b526
ヌルポ <191977649@qq.com>
2021-12-17 02:18:42 +1100

    fixed the mall objects

--- mods/deathmatch/resources/[debug]/debug/debug_s.lua
+++ mods/deathmatch/resources/[debug]/debug/debug_s.lua
@@ -1,6 +1,6 @@
 function isTransparentFlag(flag) 
     local transparentFlags = {
-        0x8
+        0x8,204084
     }
     for i,mask in ipairs(transparentFlags) do
         if bitAnd(flag,mask) ~= 0 then 
@@ -10,4 +10,4 @@ function isTransparentFlag(flag)
     return false
 end
 
-print(isTransparentFlag(360) )
+print(isTransparentFlag(2097284) )
