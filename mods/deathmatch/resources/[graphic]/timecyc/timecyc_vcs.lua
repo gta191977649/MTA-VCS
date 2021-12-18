cf82e4638c84308c31c7ad68351b09c927aa4e3e
ヌルポ <episodes27@gmail.com>
2021-12-19 03:04:32 +0800

    add

--- mods/deathmatch/resources/[graphic]/timecyc/timecyc_vcs.lua
+++ mods/deathmatch/resources/[graphic]/timecyc/timecyc_vcs.lua
@@ -155,7 +155,7 @@ function setWeatherFromTimecyc(weather_id,hour,min)
     local endTntervalIndex = intervalIndex + 1 > #Timecyc[weather_id] and 1 or intervalIndex + 1
     local WT_E = Timecyc[weather_id][endTntervalIndex]
     -- color filter 48
-    setColorFilter (WT_S[1], WT_S[2], WT_S[3],30, WT_S[1], WT_S[2], WT_S[3], 30)
+    setColorFilter (WT_S[1], WT_S[2], WT_S[3],40, WT_S[1], WT_S[2], WT_S[3],40)
 
     -- sky gradient
     local skyTopGradient = getGradientInterpolationValue({WT_S[16],WT_S[17],WT_S[18]},{WT_E[16],WT_E[17],WT_E[18]},hour,min) -- SkytopGradient
