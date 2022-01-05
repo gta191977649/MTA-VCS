e997326cf6485b0d7df077c0b9d948570b2a41aa
ヌルポ <191977649@qq.com>
2022-01-06 00:54:30 +0800

    add

--- mods/deathmatch/resources/[graphic]/fx/fx.lua
+++ mods/deathmatch/resources/[graphic]/fx/fx.lua
@@ -1,6 +1,20 @@
 FX = {
     textures = {
         ["coronastar"] = "src/coronastar.png",
+        ["cloud1"] = "src/cloud1.png",
+        ["cloud2"] = "src/cloud2.png",
+        ["cloud3"] = "src/cloud3.png",
+        ["cloudhilit"] = "src/cloudhilit.png",
+        ["cloudmasked"] = "src/cloudmasked.png",
+        ["cloudhigh"] = "src/cloudmasked.png",
+        ["collisionsmoke"] = "src/collisionsmoke.png",
+        ["waterclear256"] = "src/waterclear256.png",
+        ["waterwake"] = "src/waterwake.png",
+        ["waterreflection2"] = "src/waterreflection2.png",
+        ["collisionsmoke"] = "src/collisionsmoke.png",
+        ["pointlight"] = "src/pointlight.png",
+        ["smoke"] = "src/smoke.png",
+        ["waterspark_16"] = "src/waterspark_16.png",
     },
     total = 62,
     size = 100 -- 100x100
--- mods/deathmatch/resources/[graphic]/water/fx.lua
+++
@@ -1,9 +0,0 @@
-local water_shader = dxCreateShader( "fx/water_vc.fx")
-local water = dxCreateTexture( "images/waterclear256.png")
-dxSetShaderValue (water_shader, "Tex0", water)
-engineApplyShaderToWorldTexture(water_shader, "waterclear256")
-
-local water_wake = dxCreateShader( "fx/water_vc.fx")
-local wake = dxCreateTexture( "images/waterwake.png")
-dxSetShaderValue (water_wake, "Tex0", wake)
-engineApplyShaderToWorldTexture(water_wake, "waterwake")
