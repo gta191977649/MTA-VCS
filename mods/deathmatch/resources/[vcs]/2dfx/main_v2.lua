d5dafab713098a4fafabf46133e9b19172c2f4f3
ヌルポ <191977649@qq.com>
2022-01-06 23:36:09 +0800

    Update main_v2.lua

--- mods/deathmatch/resources/[vcs]/2dfx/main_v2.lua
+++ mods/deathmatch/resources/[vcs]/2dfx/main_v2.lua
@@ -1,4 +1,6 @@
+DGS = exports.dgs
 FADE_DIST = 100
+DEBUG = true
 LIGHT = exports.dl_lightmanager
 
 LIGHT_OBJ_NAMES = {
@@ -14,21 +16,79 @@ LIGHT_TRAFFIC_NAMES = {
 }
 LIGHT_OBJS = {}
 TRAFFIC_OBJS = {}
+TRAFFIC_LIGHT_COLORS = {
+    ["YELLOW"] = {255,234,0,180},
+    ["RED"] = {255,0,0,180},
+    ["GREEN"] = {0,255,0,180},
+}
+TRAFFIC_LIGHT_COLOR_MAPPING = {
+    ["NS"] = {
+        [0] = TRAFFIC_LIGHT_COLORS["GREEN"],
+        [5] = TRAFFIC_LIGHT_COLORS["GREEN"],
+        [8] = TRAFFIC_LIGHT_COLORS["GREEN"],
+        [2] = TRAFFIC_LIGHT_COLORS["RED"],
+        [3] = TRAFFIC_LIGHT_COLORS["RED"],
+        [4] = TRAFFIC_LIGHT_COLORS["RED"],
+        [1] = TRAFFIC_LIGHT_COLORS["YELLOW"],
+        [6] = TRAFFIC_LIGHT_COLORS["YELLOW"],
+        [7] = TRAFFIC_LIGHT_COLORS["YELLOW"],
+    },
+    ["WE"] = {
+        [3] = TRAFFIC_LIGHT_COLORS["GREEN"],
+        [5] = TRAFFIC_LIGHT_COLORS["GREEN"],
+        [7] = TRAFFIC_LIGHT_COLORS["GREEN"],
+        [0] = TRAFFIC_LIGHT_COLORS["RED"],
+        [1] = TRAFFIC_LIGHT_COLORS["RED"],
+        [2] = TRAFFIC_LIGHT_COLORS["RED"],
+        [4] = TRAFFIC_LIGHT_COLORS["YELLOW"],
+        [6] = TRAFFIC_LIGHT_COLORS["YELLOW"],
+        [8] = TRAFFIC_LIGHT_COLORS["YELLOW"],
+    },
+}
+
+
 TOTAL = 0
 RENDERED = 0
 IS_NIGHT = false
 local h,m = getTime()
+local traffic_status = getTrafficLightState()
+
+function getTrafficLightDirection(rotation) 
+    local angleMapping = {
+        ["NS"] = {
+            {0,45},
+            {135,180},
+            {180,225},
+            {315,360},
+        }
+    }
+    for _,bounds in ipairs(angleMapping["NS"]) do 
+        if rotation >= bounds[1] and rotation <= bounds[2] then 
+            return "NS"
+        end
+    end
+    return "WE"
+end
+
 function applyTrafficLightPatch(object,modelname) 
     for k,v in pairs(LIGHT_TRAFFIC_NAMES) do
         if string.find(modelname,k) then 
             local x,y,z = getElementPosition(object)
             local _,_,rz = getElementRotation(object)
-            TRAFFIC_OBJS[object] = {
+            LIGHT_OBJS[object] = {
+                type = "Traffic",
                 pos = {x,y,z},
-                rot = rz,
+                rot = getTrafficLightDirection(rz),
                 light = nil,
                 isdamage = false,
+                color = {255,0,0,180},
             }
+            if debug then 
+                local r,g,b,a = unpack(TRAFFIC_LIGHT_COLOR_MAPPING[LIGHT_OBJS[object].rot][traffic_status])
+                LIGHT_OBJS[object].label = DGS:dgsCreate3DText(x,y,z,getTrafficLightDirection(rz) ,tocolor(r,g,b,a))
+                attachElements(LIGHT_OBJS[object].label,object)
+            end
+            
             TOTAL = TOTAL + 1
             return
         end
@@ -39,6 +99,7 @@ function applyLightShadowPatch(object,modelname)
         if string.find(modelname,k) then 
             local x,y,z = getElementPosition(object)
             LIGHT_OBJS[object] = {
+                type = "Street",
                 pos = {x,y,z},
                 light = nil,
                 isdamage = false,
@@ -53,19 +114,32 @@ function applyLightShadowPatch(object,modelname)
 end
 
 function create2dFX(object) 
-    if LIGHT_OBJS[object] and LIGHT_OBJS[object].light == nil then
-        local x,y,z = unpack(LIGHT_OBJS[object].pos)
-        local offset = LIGHT_OBJS[object].offset
-        local r,g,b,a = unpack(LIGHT_OBJS[object].color)
-        local acc = LIGHT_OBJS[object].accu
-        LIGHT_OBJS[object].light = LIGHT:createPointLight(x,y,z+offset,r,g,b,a,acc,false)
-        LIGHT:setLightDistFade(LIGHT_OBJS[object].light,FADE_DIST,FADE_DIST*0.6)
-        LIGHT_OBJS[object].damage = false
-        RENDERED = RENDERED + 1
+    if LIGHT_OBJS[object].light then return end
+    if LIGHT_OBJS[object] then
+        if LIGHT_OBJS[object].type == "Street" then -- street
+            local x,y,z = unpack(LIGHT_OBJS[object].pos)
+            local offset = LIGHT_OBJS[object].offset
+            local r,g,b,a = unpack(LIGHT_OBJS[object].color)
+            local acc = LIGHT_OBJS[object].accu
+            LIGHT_OBJS[object].light = LIGHT:createPointLight(x,y,z+offset,r,g,b,a,acc,false)
+            LIGHT:setLightDistFade(LIGHT_OBJS[object].light,FADE_DIST,FADE_DIST*0.6)
+            LIGHT_OBJS[object].damage = false
+            RENDERED = RENDERED + 1
+        end
+        if LIGHT_OBJS[object].type == "Traffic" then -- traffic light
+            local x,y,z = unpack(LIGHT_OBJS[object].pos)
+            local heading = LIGHT_OBJS[object].rot
+            local r,g,b,a = unpack(LIGHT_OBJS[object].color)
+
+            LIGHT_OBJS[object].light = LIGHT:createPointLight(x,y,z,r,g,b,a,10,false)
+            LIGHT:setLightDistFade(LIGHT_OBJS[object].light,FADE_DIST,FADE_DIST*0.6)
+            LIGHT_OBJS[object].damage = false
+            RENDERED = RENDERED + 1
+        end
     end
 end
 function remove2dFX(object) 
-    if LIGHT_OBJS[object] and LIGHT_OBJS[object].light ~= nil then -- recreate damaged light
+    if LIGHT_OBJS[object] and LIGHT_OBJS[object].light then -- recreate damaged light
         LIGHT:destroyLight(LIGHT_OBJS[object].light)
         LIGHT_OBJS[object].light = nil
         RENDERED = RENDERED - 1
@@ -85,11 +159,14 @@ function forceDestory()
         end
     end
 end
-
+function updateTrafficLights() 
+    
+end 
 function init() 
     for k,v in ipairs(getElementsByType("object")) do
         local name = getElementID(v)
         applyLightShadowPatch(v,name) 
+        applyTrafficLightPatch(v,name) 
     end
     -- first render 2dfx
     if IS_NIGHT then forceRender() end
@@ -109,9 +186,8 @@ addEventHandler( "onClientElementStreamIn", root,
         if not IS_NIGHT then return end
         --if not IS_NIGHT then return end -- skip if is day
         if getElementType(source) == "object" and LIGHT_OBJS[source] then
-            if not LIGHT_OBJS[source].light then -- recreate damaged light
-                create2dFX(source) 
-            end
+            if LIGHT_OBJS[source].light then return end
+            create2dFX(source) 
         end
     end
 )
@@ -138,6 +214,7 @@ addEventHandler("onClientRender",root,function()
     newh,newm = getTime()
     if m ~= newm then
         h,m = newh,newm
+        -- light toogle
         if h > 7 and h < 21 then -- day
             if IS_NIGHT then 
                 lightSwitch(false)
@@ -150,7 +227,12 @@ addEventHandler("onClientRender",root,function()
                 IS_NIGHT = true
             end
         end
-       
+        -- traffic light direction 
+        local new_status = getTrafficLightState()
+        if traffic_status ~= new_status then
+            print(new_status)
+            traffic_status = new_status
+        end
     end
 end)
 
